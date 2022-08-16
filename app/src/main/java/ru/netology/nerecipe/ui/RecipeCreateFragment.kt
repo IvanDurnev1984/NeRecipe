df9c8489f6f8ea1f7867b1e52c783953fdb3b83c
IvanDurnev1984 <comp-1984@yandex.ru>
2022-08-16 23:14:25 +0400

    Initial commit

---
+++ app/src/main/java/ru/netology/nerecipe/ui/RecipeCreateFragment.kt
@@ -0,0 +1,69 @@
+package ru.netology.nerecipe.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nerecipe.R
+import ru.netology.nerecipe.databinding.FragmentCreateBinding
+import ru.netology.nerecipe.viewModel.RecipeViewModel
+
+class RecipeCreateFragment : Fragment() {
+    private val viewModel by activityViewModels<RecipeViewModel>()
+    private var categoryRecipeNumber = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentCreateBinding.inflate(layoutInflater, container, false).also { binding ->
+
+        binding.buttonSave.setOnClickListener {
+            onSaveButtonClicked(binding)
+        }
+
+        binding.categoryRecipeCheckBox.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                R.id.checkBoxEuropean -> categoryRecipeNumber = "Европейская кухня"
+                R.id.checkBoxAsian -> categoryRecipeNumber = "Азиатская кухня"
+                R.id.checkBoxPanasian -> categoryRecipeNumber = "Паназиатская кухня"
+                R.id.checkBoxEastern -> categoryRecipeNumber = "Восточная кухня"
+                R.id.checkBoxAmerican -> categoryRecipeNumber = "Американская кухня"
+                R.id.checkBoxRussian -> categoryRecipeNumber = "Русская кухня"
+                R.id.checkBoxMediterranean -> categoryRecipeNumber = "Средиземноморская кухня"
+            }
+        }
+
+        binding.buttonSave.setOnClickListener {
+            onSaveButtonClicked(binding)
+        }
+    }.root
+
+    private fun onSaveButtonClicked(binding: FragmentCreateBinding) {
+
+        val title = binding.title.text.toString()
+        val authorName = binding.authorName.text.toString()
+        val textRecipe = binding.textRecipe.text.toString()
+
+        if (!emptyCheckUpdateWarning(title = title, authorName = authorName, textRecipe = textRecipe, categoryRecipe = categoryRecipeNumber)) return
+
+        viewModel.onSaveClicked(title = title, authorNam = authorName, categoryRecipe = categoryRecipeNumber, textRecipe = textRecipe)
+        findNavController().popBackStack()
+    }
+
+    private fun emptyCheckUpdateWarning(
+        title: String,
+        authorName: String,
+        textRecipe: String,
+        categoryRecipe: String
+    ): Boolean {
+        return if (title.isBlank() || authorName.isBlank() || textRecipe.isBlank() || categoryRecipe.isBlank()) {
+            Toast.makeText(activity, "Все поля должны быть заполнены", Toast.LENGTH_LONG).show()
+            false
+        } else true
+    }
+
+}
---
+++ app/src/main/java/ru/netology/nerecipe/ui/RecipeFilterFragment.kt
@@ -0,0 +1,68 @@
+package ru.netology.nerecipe.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nerecipe.databinding.FragmentFilterBinding
+import ru.netology.nerecipe.viewModel.RecipeViewModel
+
+class RecipeFilterFragment : Fragment() {
+    private val viewModel by activityViewModels<RecipeViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentFilterBinding.inflate(layoutInflater, container, false).also { binding ->
+
+        binding.buttonApply.setOnClickListener {
+            onApplyButtonClicked(binding)
+        }
+    }.root
+
+
+    private fun onApplyButtonClicked(binding: FragmentFilterBinding) {
+        var checkedCount = 0
+        val numberOfFilters = 7
+
+        if (!binding.checkBoxEuropean.isChecked) {
+            ++checkedCount
+            viewModel.showEuropean("Европейская кухня")
+        }
+        if (!binding.checkBoxAsian.isChecked) {
+            ++checkedCount
+            viewModel.showAsian("Азиатская кухня")
+
+        }
+        if (!binding.checkBoxPanasian.isChecked) {
+            ++checkedCount
+            viewModel.showPanasian("Паназиатская кухня")
+
+        }
+        if (!binding.checkBoxEastern.isChecked) {
+            ++checkedCount
+            viewModel.showEastern("Восточная кухня")
+        }
+        if (!binding.checkBoxAmerican.isChecked) {
+            ++checkedCount
+            viewModel.showAmerican("Американская кухня")
+        }
+        if (!binding.checkBoxRussian.isChecked) {
+            ++checkedCount
+            viewModel.showRussian("Русская кухня")
+        }
+        if (!binding.checkBoxMediterranean.isChecked) {
+            ++checkedCount
+            viewModel.showMediterranean( "Средиземноморская кухня")
+        }
+
+        if (checkedCount == numberOfFilters) {
+            Toast.makeText(activity, "Вы не можете отключить все фильтры", Toast.LENGTH_LONG).show()
+            return
+        } else findNavController().popBackStack()
+    }
+
+}
